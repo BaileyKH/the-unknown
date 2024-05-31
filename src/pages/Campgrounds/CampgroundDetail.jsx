da008a134dfc9a6193294eddae4a99215a55050d
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-05-31 15:15:23 -0700

    styled individual camp site content

--- src/pages/Campgrounds/CampgroundDetail.jsx
+++ src/pages/Campgrounds/CampgroundDetail.jsx
@@ -31,44 +31,50 @@ export const CampgroundDetail = () => {
         loadGround()
     },[id])
 
-    
-
     return(
         <div className={`${darkMode && "dark"}`}>
-            <div>
                 {groundsDetails && (
-                    <div>
-                        <img src={groundsDetails.images[0].url ? groundsDetails.images[0].url : unavailable}/>
-                        <div className="flex items-center justify-center">
-                            <img src={darkMode ? dCampfire : lCampfire} className="w-12"/>
-                            <h1 className="text-4xl font-bold text-color">{groundsDetails.name}</h1>
-                            <img src={darkMode ? dCampfire : lCampfire} className="w-12"/>
+                    <div className="mx-8">
+                        <img src={groundsDetails.images[0].url ? groundsDetails.images[0].url : unavailable} className="rounded-b-xl w-full"/>
+                        <div className="flex items-center justify-center mt-4 md:mt-8 mb-2 md:mb-4">
+                            <img src={darkMode ? dCampfire : lCampfire} className="w-6 md:w-12 drop-shadow-lg"/>
+                            <h1 className="text-lg md:text-4xl font-bold text-color text-center mx-2 drop-shadow-lg">{groundsDetails.name}</h1>
+                            <img src={darkMode ? dCampfire : lCampfire} className="w-6 md:w-12 drop-shadow-lg"/>
                         </div>
-                        <p>{groundsDetails.description}</p>
-                        <p>Weather: {groundsDetails.weatherOverview}</p>
+                        <div className="flex flex-col justify-center items-start">
+                            <p className="text-color text-xs md:text-lg md:px-4 text-center">{groundsDetails.description}</p>
+                            <p className="campground-text mt-10 md:mt-16"><span className="campground-span">Weather:</span> {groundsDetails.weatherOverview}</p>
 
-                        <p>Total campsites: {groundsDetails.campsites.totalSites}</p>
+                            <p className="campground-text mt-8 md:mt-16"><span className="campground-span">Total campsites:</span> {groundsDetails.campsites.totalSites}</p>
 
-                        <h2>Fees</h2>
-                        {groundsDetails.fees.map(detail => (
-                            <div key={groundsDetails.id}>
-                                <p>{detail.title}</p>
-                                <p>{detail.cost}</p>
-                                <p>{detail.description}</p>
+                            <p className="campground-span mt-8 md:mt-16">Fees</p>
+                            {groundsDetails.fees.map(detail => (
+                                <div>
+                                    <p className="campground-text underline underline-offset-4 mt-4 md:mt-6">{detail.title}</p>
+                                    <p className="campground-text mt-1 md:mt-2">${detail.cost}</p>
+                                    <p className="campground-text mt-1 md:mt-2">{detail.description}</p>
+                                </div>
+                            ))}
+
+                            <p className="campground-span mt:8 md:mt-16">Amenities</p>
+                            <p className="campground-text mt-4 md:mt-6">Restroom: {groundsDetails.amenities.toilets}</p>
+                            <p className="campground-text mt-2 md:mt-4">Showers: {groundsDetails.amenities.showers}</p>
+                            <p className="campground-text mt-4">Firewood for sale: {groundsDetails.amenities.firewoodForSale}</p>
+
+                            <p className="campground-span mt-8 md:mt-16">Directions</p>
+                            <p className="campground-text mt-4 md:mt-6">Address: {groundsDetails.addresses[0] ? `${groundsDetails.addresses[0].line1} ${groundsDetails.addresses[0].city},${groundsDetails.addresses[0].stateCode} ${groundsDetails.addresses[0].postalCode}` : "Address not Available"}</p>
+                            <p className="campground-text mt-2 md:mt-4">{groundsDetails.directionsOverview}</p>
+
+                            <div className="flex flex-col justify-center items-center">
+                                <a href={groundsDetails.reservationUrl} target="_blank" className="mt-10 md:mt-16 px-2 md:px-4 py-1 md:py-2 font-bold text-lg md:text-xl text-color bg-lime-500 dark:bg-amber-700/60 rounded-lg">
+                                    Book Now!
+                                </a>
+                                <p className="text-xs text-center md:text-sm text-color mt-4 md:mt-6">{groundsDetails.regulationsOverview}</p>
                             </div>
-                        ))}
-
-                        <h2>Amenities</h2>
-                        <p>Restroom: {groundsDetails.amenities.toilets}</p>
-                        <p>Showers: {groundsDetails.amenities.showers}</p>
-                        <p>Firewood for sale: {groundsDetails.amenities.firewoodForSale}</p>
-
-                        <h2>Directions</h2>
-                        <p>Address: {groundsDetails.addresses[0].line1} {groundsDetails.addresses[0].city},{groundsDetails.addresses[0].stateCode} {groundsDetails.addresses[0].postalCode}</p>
-                        <p>{groundsDetails.directionsOverview}</p>
+                        </div>
+                        
                     </div>
                 )}
-            </div>
         </div>
     );
 }
