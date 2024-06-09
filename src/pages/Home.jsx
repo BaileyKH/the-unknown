ebd9eda5d56d168f6e827ab28ce4c1f3bebc5e00
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-06-09 11:04:59 -0700

    home comp adjustment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { Link, useOutletContext } from "react-router-dom";
 
 import { IconExclamationMark } from '@tabler/icons-react';
@@ -27,18 +26,18 @@ import { Trending } from "../components/Trending";
 
 
 export const Home = () => {
+
     const { darkMode } = useOutletContext();
 
-    const heroImage = useMemo(() => (darkMode ? darkHero : heroImg), [darkMode]);
-    const altText = useMemo(() => (darkMode ? "Spooky forest with tent" : "Sunny forest with tent"), [darkMode]);
-
-    return (
+    return(
         <div className={`${darkMode && "dark"}`}>
             <div className="w-full">
                 <div className="flex items-center justify-center">
-                    <img src={heroImage} alt={altText} />
+                    <img src={darkMode ? darkHero : heroImg} 
+                        alt={darkMode ? "Spooky forest with tent" : "Sunny forest with tent"}
+                    />
                     <div className="absolute z-10 bg-white/90 dark:bg-black/90 rounded-md px-4 md:px-8 py-4 mx-4 drop-shadow-2xl">
-                        <IconExclamationMark stroke={2} className="absolute animate-bounce -left-2 -top-2 text-black bg-yellow-300 drop-shadow-2xl rounded-full" />
+                        <IconExclamationMark stroke={2} className="absolute animate-bounce -left-2 -top-2 text-black bg-yellow-300 drop-shadow-2xl rounded-full"/>
                         <h1 className="text-color text-xl md:text-4xl lg:text-6xl font-bold drop-shadow-md">
                             Adventure into the Unknown
                         </h1>
